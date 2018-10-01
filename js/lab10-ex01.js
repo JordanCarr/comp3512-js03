57f6b0980ee81cac4631cca6f338a58bd6544916
Jordan Carr <JordanCarr@users.noreply.github.com>
2018-10-01 15:40:25 -0600

    attempted exercise 1

--- js/lab10-ex01.js
+++ js/lab10-ex01.js
@@ -1,5 +1,18 @@
-window.addEventListener('load', function() {
-    
+window.addEventListener("load", function () {
+    fetch("https://api.github.com/orgs/funwebdev-2nd-ed/repos")
+        .then(response => {
+            let result;
 
-    
+            if (response.ok) {
+                result = response.json();
+            } else {
+                result = Promise.reject({
+                                            status: response.status,
+                                            statusText: response.statusText
+                                        });
+            }
+
+            return result;
+        })
+        .then((data) => { console.dir(data); });
 });
