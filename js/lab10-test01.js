90e96b6e0472ff88b37fefcc6f9a6225e63834d7
Jordan Carr <JordanCarr@users.noreply.github.com>
2018-10-01 15:43:13 -0600

    attempted test 01

--- js/lab10-test01.js
+++ js/lab10-test01.js
@@ -1,8 +1,37 @@
 // before you start, view this url in the browser to see its sructure
-const url = 'https://gist.githubusercontent.com/rconnolly/1d6ac7ede49e501ff0aca8a0c2a36c8c/raw/d21fadafd1c48eb3ab6d507993ebf5c6288a28c7/continents.json';
+const url = "https://gist.githubusercontent.com/rconnolly/1d6ac7ede49e501ff0aca8a0c2a36c8c/"
+            + "raw/d21fadafd1c48eb3ab6d507993ebf5c6288a28c7/continents.json";
 
-window.addEventListener('load', function() {
-    
+window.addEventListener("load", function () {
+
     // fetch the continents from the api in url
-    
+    fetch(url)
+        .then(response => {
+            let result;
+
+            if (response.ok) {
+                result = response.json();
+            } else {
+                result = Promise.reject({
+                                            status: response.status,
+                                            statusText: response.statusText
+                                        });
+            }
+
+            return result;
+        })
+        .catch(error => console.log(error))
+        .then(data => addContinentsToUL(data));
 });
+
+function addContinentsToUL(data) {
+    let list = document.querySelector("ul");
+
+    for (let continent of data) {
+        let continentNode = document.createElement("LI");
+        let nameNode = document.createTextNode(continent.name);
+        continentNode.appendChild(nameNode);
+
+        list.appendChild(continentNode);
+    }
+}
